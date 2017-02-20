526d0ecf3709fb8bf78f56e571f23db55c454eb6
HungMingWu <u9089000@gmail.com>
2017-02-20 09:29:27 +0000

    Update

--- go_epub.go
+++ go_epub.go
@@ -1,54 +1,78 @@
 package main
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io/ioutil"
-        "os"
-        "log"
-        "os/exec"
+	"os"
+	"os/exec"
+	"strings"
 )
 
-func main() {
-//      https://github.com/jacpy/GoDemo
-//	url := "http://www.kancloud.cn/digest/hevc-fred/"
-	url := "http://www.kancloud.cn/digest/hevc-fred/181912"
-        p, err := goquery.NewDocument(url)
-        if err != nil {
-                panic(err)
-        }
-        fileInfoArr, err := ioutil.ReadDir("html")
-        log.Println(fileInfoArr, err)
-        if len(fileInfoArr) == 0 || err != nil && os.IsNotExist(err) {
-                os.Mkdir("html", os.ModePerm)
-                log.Println(err)
-        }
-        current, _ := os.Getwd()
-        dir := current + "/html"
-//	p.Find("div.tab-content").Find("ul.directory-list li").Each(func(idx int, selection *goquery.Selection) {
-//                href, exists := selection.Find("a").Attr("href")
-//		href = "http://www.kancloud.cn" + href
-//                log.Println("exists: ", exists, "href: ", href, "idx: ", idx)
-//        })
-        p.Find("script").Remove()
+func fetchHtml(url string, outputName string) {
+	p, err := goquery.NewDocument(url)
+	if err != nil {
+		panic(err)
+	}
+	p.Find("script").Remove()
 	p.Find("div.manual-left").Remove()
-        p.Find("div.think-loading").Remove()
+	p.Find("div.think-loading").Remove()
 	p.Find("div.article-head").Remove()
-        p.Find("div.manual-head").Remove()
-        p.Find("div.view-foot").Remove()
-        content, err := p.Html()
-	savefile := dir + "/output.html"
-        err = ioutil.WriteFile(savefile, []byte(content), os.ModePerm)
-        if err != nil {
-                panic(err)
-        }
-        args := make([]string, 3)
-        args[0] = "html/output.html"
-        args[1] = "-o"
-        args[2] = "output.md"
-        cmd := exec.Command("pandoc", args...)
-        err = cmd.Run()
-        if err != nil {
-                panic(err)
-        }
-	
+	p.Find("div.manual-head").Remove()
+	p.Find("div.view-foot").Remove()
+	content, err := p.Html()
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(outputName, []byte(content), os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func translateFromHtmlToMarkdown(input string, output string) {
+	args := make([]string, 3)
+	args[0] = input
+	args[1] = "-o"
+	args[2] = output
+	cmd := exec.Command("pandoc", args...)
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	//      https://github.com/jacpy/GoDemo
+	url := "http://www.kancloud.cn/digest/hevc-fred/"
+	p, err := goquery.NewDocument(url)
+	if err != nil {
+		panic(err)
+	}
+	_, err = ioutil.ReadDir("html")
+	if os.IsNotExist(err) {
+		os.Mkdir("html", os.ModePerm)
+	}
+	_, err = ioutil.ReadDir("md")
+	if os.IsNotExist(err) {
+		os.Mkdir("md", os.ModePerm)
+	}
+	current, _ := os.Getwd()
+	summary := "# 目录"
+	summary = summary + "\n\n"
+	p.Find("div.tab-content").Find("ul.directory-list li").Each(func(idx int, selection *goquery.Selection) {
+		title := strings.TrimSpace(selection.Text())
+		href, _ := selection.Find("a").Attr("href")
+		href = "http://www.kancloud.cn" + href
+		strIdx := fmt.Sprint(idx)
+		chapHtml := current + "/html/chap" + strIdx + ".html"
+		chapMD := current + "/md/chap" + strIdx + ".md"
+		fetchHtml(href, chapHtml)
+		translateFromHtmlToMarkdown(chapHtml, chapMD)
+		summary = summary + "* [" + title + "](chap" + strIdx + ".md)\n"
+	})
+	err = ioutil.WriteFile(current+"/md/"+"SUMMARY.md", []byte(summary), os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
